518accd5bb6483df5a27868125d7bb1b9950bf41
colbywon <colbyjmiller@gmail.com>
2019-09-24 19:41:05 -0600

    init navigation.js update endor

---
+++ Navigation.js
@@ -0,0 +1,22 @@
+document.write("  <ul>
+    <li><a href="home.html">Home</a></li>
+    <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Medium</a>
+      <div class="dropdown-content">
+        <a href="Medium Home.html">Homepage</a>
+        <a href="Music.html">Music</a>
+        <a href="">Concepts</a>
+        <a href="">Credits</a>
+      </div>
+    <li><a href="drawings">Drawings</a></li>
+    <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Games</a>
+      <div class="dropdown-content">
+        <a href="Crab.io.html">Crab.io</a>
+        <a href="Battle%20of%20Endor.html">Battle of Endor</a>
+        <a href="Fat%20cat.html">Fat Cat Flies</a>
+        <a href="Rasputin.html">Rar Ra Raspu Run</a>
+         <a href="Sanic.html">Sanic Jump</a>
+      </div>
+    </li>
+  </ul>");
