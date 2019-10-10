a12d58cb70b4ce5282fe9135ec451676ca0e583a
colbywon <colbyjmiller@gmail.com>
2019-10-10 17:37:58 -0600

    init stuff

---
+++ nav_bar.js
@@ -0,0 +1,26 @@
+function run() {
+  <html>
+  <ul>
+      <li><a href="/home.html">Home</a></li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Medium</a>
+        <div class="dropdown-content">
+          <a href="/Medium/Medium_Home.html">Homepage</a>
+          <a href="/Medium/Music/Music.html">Music</a>
+          <a href="">Concepts</a>
+          <a href="">Credits</a>
+        </div>
+      <li><a href="drawings">Drawings</a></li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Games</a>
+        <div class="dropdown-content">
+          <a href="/Games/Crab.io.html">Crab.io</a>
+          <a href="/Games/Battle_of_Endor.html">Battle of Endor</a>
+          <a href="/Games/Fat_cat.html">Fat Cat Flies</a>
+          <a href="/Games/Rasputin.html">Rar Ra Raspu Run</a>
+          <a href="/Games/Sanic.html">Sanic Jump</a>
+        </div>
+      </li>
+    </ul>
+  </html>
+}
